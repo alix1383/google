db33f52cb3fe8e99f41ccaec0bb869c1b8c197c1
Alix <73092812+alix1383@users.noreply.github.com>
2021-05-02 11:57:39 +0430

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>google</title>
+    </head>
+    <body>
+<link rel="stylesheet" href="style.css" type="text/css">
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+        <div class="g"id="red"></div>
+                  <div class="g"id="yellow"></div>
+                  <div class="g"id="green"></div>
+                  <div id="blue"></div>
+                  <div class="g"id="blueblue"></div>
+    </body>
+</html>
+
+
