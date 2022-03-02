5ad87fbab806b94d988398ecb567d35e31b15215
Alix <73092812+alix1383@users.noreply.github.com>
2022-03-02 18:24:49 +0330

    Update index.php

--- index.php
+++ index.php
@@ -4,13 +4,20 @@
         <title>google</title>
     </head>
     <body>
-<link rel="stylesheet" href="style.css" type="text/css">
-<link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+
+        <link rel="stylesheet" href="style.css" type="text/css">
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon"/>
+
         <div class="g"id="red"></div>
-                  <div class="g"id="yellow"></div>
-                  <div class="g"id="green"></div>
-                  <div id="blue"></div>
-                  <div class="g"id="blueblue"></div>
+  
+        <div class="g"id="yellow"></div>
+  
+        <div class="g"id="green"></div>
+  
+        <div id="blue"></div>
+  
+        <div class="g"id="blueblue"></div>
+
     </body>
 </html>
 
